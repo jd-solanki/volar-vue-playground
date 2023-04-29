3b059e9099041d41b25d1e4345b91bae46fc8d5c
JD Solanki <jdsolanki0001@gmail.com>
2023-04-30 00:01:34 +0530

    pushing

--- src/components/foo/Play.vue
+++ src/components/foo/Play.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import Foo from './components/bar/Bar.vue';
-import Bar from './components/foo/Foo.vue';
+import Foo from './Foo.vue';
 </script>
 
 <template>
   <p>Awesome</p>
   <Foo type="awesome"></Foo>
   <hr>
-  <Bar></Bar>
 </template>
