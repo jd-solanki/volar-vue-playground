b7fb206eb1894b02a1c17df00fed21040fa794fa
JD Solanki <jdsolanki0001@gmail.com>
2023-05-06 14:01:16 +0530

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-
+import Foo from './foo/Foo.vue';
 </script>
 
 <template>
   <p>Awesome</p>
+  <Foo #ke>
+    awesome
+  </Foo>
 </template>
---
+++ src/foo/Foo.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+defineSlots<{ 'kebabCase'?: (_: any) => any }>()
+</script>
+
+<template>
+    <div class="foo">
+        <slot name="kebabCase"></slot>
+    </div>
+</template>
